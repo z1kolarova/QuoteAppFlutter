8f6a870647927ae451c6c4a48052ede5955ad203
Zdeni <z1_kolarova@utb.cz>
2023-03-03 09:10:41 +0100

    Deleting and refreshing

--- quote_app_flutter/lib/screens/all_quotes_screen.dart
+++ quote_app_flutter/lib/screens/all_quotes_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quote_app_flutter/db/quotes_repository.dart';
-import 'package:quote_app_flutter/screens/components/all_quotes_listview.dart';
 import 'package:quote_app_flutter/screens/quote_edit_screen.dart';
 import 'package:quote_app_flutter/styles/styles.dart';
 
@@ -31,15 +30,64 @@ class AllQuotesScreenState extends State<AllQuotesScreen> {
             builder: (BuildContext context,
                 AsyncSnapshot<List<QuoteRecord>> snapshot) {
               if (snapshot.hasData) {
-                return AllQuotesListView(items: snapshot.data!);
+                return buildAllQuotesListView(snapshot.data!);
               }
-              return const AllQuotesListView(items: <QuoteRecord>[]);
+              return buildAllQuotesListView(<QuoteRecord>[]);
             },
           )), // AllQuotesListView(items: await quotes)),
           buildMenuButton('Add new quote', switchToQuoteEditingScreen)
         ],
       ),
     );
+  }
+
+  Widget buildAllQuotesListView(List<QuoteRecord> items) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(10),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Column(children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(10),
+            child: Text(items[index].text,
+                textScaleFactor: 1.5,
+                textAlign: TextAlign.left,
+                style: MyStyles.quoteStyle()),
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextButton(
+                    onPressed: () => deleteTheQuote(index),
+                    style: MyStyles.defaultButtonStyle(),
+                    child: const Text('Delete', textScaleFactor: 1.3)),
+              ),
+              Flexible(
+                  child: Container(
+                      alignment: Alignment.topRight,
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Text('- ${items[index].author}',
+                          textScaleFactor: 1.3, style: MyStyles.authorStyle())))
+            ],
+          )
+        ]);
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider();
+      },
+    );
+  }
+
+  void deleteTheQuote(int index) async {
+    quotes.then((actualQuotes) async {
+      await QuotesRepository.deleteQuote(actualQuotes[index].id);
+      setState(() {
+        actualQuotes.removeAt(index);
+      });
+    });
   }
 
   Widget buildMenuButton(String text, void Function()? onPressed) {
